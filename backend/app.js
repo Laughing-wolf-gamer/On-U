3f1fcadd02b0e660402ad142e6926bc45a44e43b
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2025-01-17 01:36:02 +0530

    Update 124

--- backend/app.js
+++ backend/app.js
@@ -80,6 +80,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 }); */
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
 export default app;
