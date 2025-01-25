efc339104c2e8e296b6bc231fc659ff7bb318edb
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2025-01-25 13:59:56 +0530

    Update 189

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // React 18 uses `createRoot`
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
-// import { positions, transitions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
+import store from './store'
+import { Provider } from 'react-redux'
+import {positions, transitions, Provider as Alertprovider} from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import { ToastProvider } from './Contaxt/ToastProvider';
 
-
-
-// Ensure the root element exists
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement); // Create the root using React 18
-  root.render(
-    <Provider store={store}>
-      <React.StrictMode>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </React.StrictMode>
-    </Provider>
-  );
-} else {
-  console.error('Root element not found!');
+const option = {
+  timeout: 2000,
+  position: positions.TOP_CENTER,
+  transition: transitions.SCALE
+ 
 }
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <Alertprovider template={AlertTemplate} {...option}>
+        <React.StrictMode >
+            <ToastProvider>
+              <App />
+            </ToastProvider>
+        </React.StrictMode>
+      </Alertprovider>
+    </Provider>,
+  document.getElementById('root')
+);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,73 +1,54 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { 
-  registeruser, 
-  getuser, 
-  resendotp, 
-  updateuser, 
-  otpverifie, 
-  updatedetailsuserreducer, 
-  loginuser, 
-  addressupdate, 
-  getAddress 
-} from './Reducer/userreducer';
-import { allOptions, Allproducts, randomProducts, singleProduct } from './Reducer/productreducer';
-import { 
-  create_bag_reducer, 
-  create_order_reducer, 
-  create_wishlist_reducer, 
-  delete_bag_reducer, 
-  delete_wish_reducer, 
-  get_all_order_reducer, 
-  get_bag_reducer, 
-  get_order_by_id_reducer, 
-  get_wishlist_reducer, 
-  update_qty_bag_reducer 
-} from './Reducer/orderreducer';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+import { registeruser, getuser, resendotp, updateuser, otpverifie, updatedetailsuserreducer, loginuser, addressupdate, getAddress } from './Reducer/userreducer';
+import {allOptions, Allproducts, randomProducts, singleProduct} from './Reducer/productreducer'
+import { create_bag_reducer, create_order_reducer, create_wishlist_reducer, delete_bag_reducer, delete_wish_reducer, get_all_order_reducer, get_bag_reducer, get_order_by_id_reducer, get_wishlist_reducer, update_qty_bag_reducer } from './Reducer/orderreducer';
 import { fetch_banners_reducer } from './Reducer/bannerreducer';
 import { fetch_All_Options, fetch_form_banners } from './Reducer/common.reducer';
-import { thunk } from 'redux-thunk';
+
 
 const reducer = combineReducers({
-  Registeruser: registeruser,
-  loginuser: loginuser,
-  user: getuser,
-  updateAddress: addressupdate,
-  resendotp: resendotp,
-  updateuser: updateuser,
-  userdetails: otpverifie,
-  Allproducts: Allproducts,
-  AllOptions: allOptions,
-  Sproduct: singleProduct,
-  getAllAddress: getAddress,
-  banners: fetch_banners_reducer,
-  wishlist: create_wishlist_reducer,
-  wishlist_data: get_wishlist_reducer,
-  bag: create_bag_reducer,
-  bag_data: get_bag_reducer,
-  update_bag: update_qty_bag_reducer,
-  updateuser2: updatedetailsuserreducer,
-  deletebagReducer: delete_bag_reducer,
-  deletewish: delete_wish_reducer,
-  createOrder: create_order_reducer,
-  getOrderById: get_order_by_id_reducer,
-  getallOrders: get_all_order_reducer,
-  RandomProducts: randomProducts,
-  fetchFormBanners: fetch_form_banners,
-  allOptions: fetch_All_Options,
-});
+      Registeruser: registeruser,
+      
+      loginuser: loginuser,
+      user : getuser,
+      updateAddress:addressupdate,
+      resendotp: resendotp,
+      updateuser: updateuser,
+      userdetails: otpverifie,
+      Allproducts:Allproducts,
+      AllOptions:allOptions,
 
-const initialState = {};
+      Sproduct: singleProduct,
+      getAllAddress:getAddress,
+      banners:fetch_banners_reducer,
+      wishlist:create_wishlist_reducer,
+      wishlist_data:get_wishlist_reducer,
+      bag:create_bag_reducer,
+      bag_data:get_bag_reducer,
+      update_bag: update_qty_bag_reducer,
+      updateuser2:updatedetailsuserreducer,
+      deletebagReducer:delete_bag_reducer,
+      deletewish:delete_wish_reducer,
 
-const middleware = [thunk];  // Use the named 'thunk' export
 
-// Only enable DevTools in development mode
-const store = createStore(
-  reducer,
-  initialState,
-  process.env.REACT_APP_NODE_ENV === 'development'
-    ? composeWithDevTools(applyMiddleware(...middleware))  // Enable DevTools if in development mode
-    : applyMiddleware(...middleware)  // Apply middleware normally otherwise
-);
+      createOrder: create_order_reducer,
+      getOrderById:get_order_by_id_reducer,
+      getallOrders:get_all_order_reducer,
 
-export default store;
+      RandomProducts:randomProducts,
+
+      fetchFormBanners:fetch_form_banners,
+      allOptions:fetch_All_Options,
+      
+})
+
+let initialState = {};
+
+const middleware  = [thunk];
+
+const store = createStore(reducer,initialState,composeWithDevTools( applyMiddleware(...middleware)))
+
+
+export default store
