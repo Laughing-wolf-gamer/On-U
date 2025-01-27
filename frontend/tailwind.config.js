4a545a7bd5c642aeba0337b1f990be1f58fe03e4
Abhishek sinha <laughingwolfgamer@gmail.com>
2025-01-27 17:23:19 +0530

    Update 14

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,22 +4,22 @@ module.exports = {
     ],
     theme: {
         extend: {
-        fontFamily: {
-            sans: ['Poppins', 'sans-serif'],
-        },
-        keyframes: {
-            vibrateScale: {
-                '0%, 100%': { transform: 'translateX(0) scale(1)' },
-                '25%': { transform: 'translateX(-2px) scale(1.1)' },
-                '50%': { transform: 'translateX(2px) scale(1.05)' },
-                '75%': { transform: 'translateX(-2px) scale(1.1)' },
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
             },
-            shineEffect: {
-                '0%': { boxShadow: '0 0 5px rgba(255, 255, 255, 0.5)' },
-                '50%': { boxShadow: '0 0 20px rgba(255, 255, 255, 1)' },
-                '100%': { boxShadow: '0 0 5px rgba(255, 255, 255, 0.5)' },
+            keyframes: {
+                vibrateScale: {
+                    '0%, 100%': { transform: 'translateX(0) scale(1)' },
+                    '25%': { transform: 'translateX(-2px) scale(1.1)' },
+                    '50%': { transform: 'translateX(2px) scale(1.05)' },
+                    '75%': { transform: 'translateX(-2px) scale(1.1)' },
+                },
+                shineEffect: {
+                    '0%': { boxShadow: '0 0 5px rgba(255, 255, 255, 0.5)' },
+                    '50%': { boxShadow: '0 0 20px rgba(255, 255, 255, 1)' },
+                    '100%': { boxShadow: '0 0 5px rgba(255, 255, 255, 0.5)' },
+                },
             },
-        },
             animation: {
                 vibrateScale: 'vibrateScale 0.4s ease-out',
                 'shine': 'shineEffect 1.5s infinite',
