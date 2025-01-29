520bf6ab1b2a23f004f441f9a7198159c0ecb4e8
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2025-01-30 01:46:55 +0530

    Update 57

--- admin-frontend/src/pages/admin-view/products.jsx
+++ admin-frontend/src/pages/admin-view/products.jsx
@@ -414,7 +414,7 @@ const AdminProducts = () => {
             </Button>
           </div>
 
-          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-5">
             {sortedProducts.length > 0 ? (
               sortedProducts.map((product, i) => (
                 <AdminProductTile key={i} togglePopUp={togglePopUp} setOpenProductPreview={setCurrentPreviewProduct} product={product} />
