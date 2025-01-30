1288b6a63b283f6a2a983e8598d65f2593649c55
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2025-01-30 22:19:19 +0530

    Update 65

--- frontend/src/components/Wishlist/Wishlist.js
+++ frontend/src/components/Wishlist/Wishlist.js
@@ -65,7 +65,7 @@ const Wishlist = () => {
     return (
         <div className="w-screen h-screen overflow-y-auto justify-start scrollbar overflow-x-hidden scrollbar-track-gray-800 scrollbar-thumb-gray-300 pb-3">
             {loadingWishList ? (
-                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:gap-6 md:gap-8 lg:gap-10 mt-6">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:gap-6 md:gap-8 lg:gap-10 mt-6 md:px-5 lg:px-6 2xl:px-6">
                     {Array(20).fill().map((_, index) => (
                         <ProductCardSkeleton key={index} />
                     ))}
@@ -73,7 +73,7 @@ const Wishlist = () => {
             ) : (
                 <Fragment>
                     {currentWishListItem && currentWishListItem.length > 0 ? (
-                        <div className="w-full sm:px-6 lg:px-10">
+                        <div className="w-full sm:px-6 lg:px-10 mt-5">
                             <h1 className="text-2xl sm:text-3xl font-semibold text-slate-800 flex justify-start items-center space-x-4">
                                 <span>My Wishlist</span>
                                 <span className="text-sm sm:text-base font-medium text-slate-500">
@@ -99,8 +99,11 @@ const Wishlist = () => {
                                             </div>
                                             
                                             {/* Remove button */}
-                                            <div className="absolute top-3 right-3 bg-gray-900 text-white rounded-full p-2 z-10 opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer group sm:hidden">
-                                                <MdClear className="text-xl sm:text-2xl" onClick={(e) => handleDelWish(e, pro?.productId?._id || pro?.productId, pro)} />
+                                            {<div className="absolute top-3 right-3 bg-gray-900 text-white rounded-full p-2 z-10 opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer group sm:block hidden">
+                                                <MdClear className="text-base sm:text-lg" onClick={(e) => handleDelWish(e, pro?.productId?._id || pro?.productId, pro)} />
+                                            </div>}
+                                            <div className="absolute top-3 right-3 bg-gray-900 text-white rounded-full p-2 z-10 opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer group sm:hidden block">
+                                                <MdClear className="text-base sm:text-lg" onClick={(e) => handleDelWish(e, pro?.productId?._id || pro?.productId, pro)} />
                                             </div>
                                         </div>
                                     </li>
