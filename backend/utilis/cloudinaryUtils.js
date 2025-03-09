f5595bc99bbd8a3d8b4dc7487993e3b95df2dd5c
Abhishek sinha <laughingwolfgamer@gmail.com>
2025-03-09 20:10:44 +0530

    Update 524

--- backend/utilis/cloudinaryUtils.js
+++ backend/utilis/cloudinaryUtils.js
@@ -9,7 +9,6 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure:process.env.NODE_ENV === 'production' ? true:false,
 });
 const storage = new multer.memoryStorage();
 /* const storage = new CloudinaryStorage({
