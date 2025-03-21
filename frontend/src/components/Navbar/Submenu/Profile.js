60faa3e4935bb53b88c1524d8f8f779a4e6ee8b5
Abhishek sinha <laughingwolfgamer@gmail.com>
2025-03-22 02:09:19 +0530

    Update 599

--- frontend/src/components/Navbar/Submenu/Profile.js
+++ frontend/src/components/Navbar/Submenu/Profile.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 import { logout } from '../../../action/useraction'
 import { useDispatch} from 'react-redux'
 import { useSettingsContext } from '../../../Contaxt/SettingsContext'
-
+import '../Navbar.css'
 const Profile = ({show, CMenu, parentCallback, user}) => {
     const dispatch = useDispatch()
     const {checkAndCreateToast} = useSettingsContext();
@@ -25,52 +25,43 @@ const Profile = ({show, CMenu, parentCallback, user}) => {
         <div>
             
             {transitions((styles, item) => item && <animated.div style={styles}>
-                <div className={`container font-kumbsan absolute  top-14 right-10  z-10 font1 max-w-[25%] w-[25%] h-[340px] ${CMenu}  Mmenu bg-gray-100 text-gray-900 cursor-pointer`}
+                <div className={`container font-kumbsan absolute  top-14 right-20 z-10 font1 max-w-[25%] w-fit h-fit ${CMenu} Mmenu bg-gray-100 justify-start items-center text-gray-900`}
                     onMouseEnter={() => parentCallback('block', true)} onMouseLeave={() => parentCallback('hidden', false)}
                 >
                     <div className='px-8 py-8 '>
                         <div className=' w-full'>
-                            <Link to='/'>
-                                <h1 className='font1 font-semibold'>Welcome</h1>
-                            </Link>
-                        {user ? "" : <h1 className='font1 font-extralight text-sm '>To access account and manage orders  <br /></h1>}
-                            
+							<h1 className='font1 font-semibold'>Welcome</h1>
+                        	{user ? "" : <h1 className='font1 font-extralight text-sm '>To access account and manage orders  <br /></h1>}
                             {
-                            user ? 
+                            user &&
                                 <div>
                                     {`${user?.user?.name}`} <br />
+									<hr className='my-2' />
                                     <Link to='/dashboard' className='font1 hover:font-semibold'>Dashboard</Link>
                                 </div>
-                                
-                                :
-                                    
-                                <Link className='dec' to="/Login">
-                                    <button className=' font1 font-semibold text-gray-900 text-sm border-[1px] px-3 py-2 
-                                    border-slate-500 hover:border-slate-900 '>LOGIN/&nbsp;SIGNUP</button>
-                                </Link>
-            
                             }
-                            <hr className='my-4' />
+                            	<hr className='my-2' />
                             	<Link to='/bag' className='litext list-none py-0.5  hover:font-semibold'>Bag</Link>
-								<Link to={`/dashboard?sideTab=Orders-Returns`} className='litext list-none py-0.5  hover:font-semibold'>
-									<h1>Order & Returns</h1>
-								</Link>
+								<hr className='my-2' />
+								<Link to={`/dashboard?sideTab=Orders-Returns`} className='litext list-none py-0.5  hover:font-semibold'><h1>Order & Returns</h1></Link>
+								<hr className='my-2' />
+								<Link to={`/dashboard?sideTab=Saved-Addresses`}><h1 className='litext list-none py-0.5 hover:font-semibold'>Saved Addresses</h1></Link>
+								<hr className='my-2' />
 								<Link to="/contact">
 								<h1 className='litext list-none py-0.5  hover:font-semibold'>Contact Us</h1>
 								</Link>
-								<Link to={`/dashboard?sideTab=Saved-Addresses`}>
-									<h1 className='litext list-none py-0.5  hover:font-semibold'>Saved Addresses</h1>
-								</Link>
-								<hr className='my-4' />
 								<br />
 
                             {
                             user ? 
-                                <button className=' font1 font-smibold text-gray-600 text-sm border-[1px] px-3 py-2 
-                                    border-[#d4d5d8] hover:border-[#2d2c2c] ' onClick={logoutBTN}>LOGOUT
+                                <button className=' font1 font-semibold text-gray-900 text-sm border-[1px] px-3 py-2 
+                                    border-slate-500 hover:border-slate-900 ' onClick={logoutBTN}>LOGOUT
                                 </button>
                                 :
-                                ""
+                                <Link className='dec' to="/Login">
+                                    <button className=' font1 font-semibold text-gray-900 text-sm border-[1px] px-3 py-2 
+                                    border-slate-500 hover:border-slate-900 '>LOGIN/&nbsp;SIGNUP</button>
+                                </Link>
                             }
             
                         </div>
