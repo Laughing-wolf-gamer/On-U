ac8797d37eeaae579011dbbe549be86c3eaa34aa
Abhishek sinha <laughingwolfgamer@gmail.com>
2025-03-31 23:39:02 +0530

    Update 677

--- frontend/src/components/Login/Dashboard/DeliveryStatus.js
+++ frontend/src/components/Login/Dashboard/DeliveryStatus.js
@@ -20,40 +20,40 @@ const DeliveryStatus = ({ status, hiddenText }) => {
 		<div className="rounded-lg mb-2 px-2 w-full">
 			<div className="w-full overflow-hidden">
 				<div className="flex items-center justify-between relative overflow-hidden">
-				{/* Progress Bar */}
-				<div className="absolute top-1/2 transform -translate-y-1/2 left-6 right-5 h-1 bg-gray-300 rounded-md">
-					<div
-					className=" h-0.5 md:h-1 bg-red-400"
-					style={{ width: `${(currentStepIndex / (steps.length - 1)) * 100}%` }}
-					></div>
-				</div>
-				{/* Steps */}
-				{steps.map((step, index) => (
-					<div key={index} className="flex flex-col items-center z-10 overflow-hidden">
+					{/* Progress Bar */}
+					<div className="absolute top-1/3 -translate-y-1/3 left-5 right-4 sm:left-4 sm:right-3 md:left-6 md:right-5 h-1 bg-gray-300 rounded-md">
 						<div
-							className={`flex flex-col items-center justify-center text-white font-bold shadow-md transition-all duration-300 text-sm sm:text-lg`}
-						>
-							<div
-								className={`rounded-full flex items-center justify-center ${
-									index <= currentStepIndex ? "bg-red-400" : "bg-gray-300"
-								} w-5 h-5 sm:w-10 sm:h-10`}
-							>
-								{step.icon}
-							</div>
-							{
-								!hiddenText && (
-									<p
-									className={`mt-2 text-xs font-semibold ${
-										index <= currentStepIndex ? "text-gray-700" : "text-gray-400"
-									}`}
-									>
-									{step.title}
-									</p>
-								)
-							}
-						</div>
+							className=" h-0.5 md:h-1 bg-red-400"
+							style={{ width: `${(currentStepIndex / (steps.length - 1)) * 100}%` }}
+						></div>
 					</div>
-				))}
+					{/* Steps */}
+					{steps.map((step, index) => (
+						<div key={index} className="flex flex-col items-center z-10 overflow-hidden">
+							<div
+								className={`flex flex-col items-center justify-center text-white font-bold shadow-md transition-all duration-300 text-sm sm:text-lg`}
+							>
+								<div
+									className={`rounded-full flex items-center justify-center ${
+										index <= currentStepIndex ? "bg-red-400" : "bg-gray-300"
+									} w-10 h-10`}
+								>
+									{step.icon}
+								</div>
+								{
+									!hiddenText && (
+										<p
+											className={`mt-2 text-xs font-semibold ${
+												index <= currentStepIndex ? "text-gray-700" : "text-gray-400"
+											}`}
+										>
+											{step.title}
+										</p>
+									)
+								}
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 			
