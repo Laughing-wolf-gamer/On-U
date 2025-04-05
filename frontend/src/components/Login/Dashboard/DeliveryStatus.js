9750cf614c7882ec7902e5315daa597e0d8e17f1
Abhishek sinha <laughingwolfgamer@gmail.com>
2025-04-05 17:11:28 +0530

    Update 728

--- frontend/src/components/Login/Dashboard/DeliveryStatus.js
+++ frontend/src/components/Login/Dashboard/DeliveryStatus.js
@@ -1,95 +1,78 @@
 import React, { useState, useEffect } from "react";
 
 const DeliveryStatus = ({ status, hiddenText }) => {
-  const steps = [
-    { title: "Confirmed", label: "Confirmed", icon: "📦" },
-    { title: "RTS", label: "Ready To Ship", icon: "🚚" },
-    { title: "Shipped", label: "Shipped", icon: "✈️" },
-    { title: "OFD", label: "Out for Delivery", icon: "📬" },
-    { title: "Delivered", label: "Delivered", icon: "✅" },
-  ];
+	const steps = [
+		{ title: "Confirmed", label: "Confirmed", icon: "📦" },
+		{ title: "RTS", label: "Ready To Ship", icon: "🚚" },
+		{ title: "Shipped", label: "Shipped", icon: "✈️" },
+		{ title: "OFD", label: "Out for Delivery", icon: "📬" },
+		{ title: "Delivered", label: "Delivered", icon: "✅" },
+	];
 
-  let currentStepIndex = steps.findIndex((step) => step.label === status);
+	let currentStepIndex = steps.findIndex((step) => step.label === status);
 
-  // Handle invalid status (i.e., not found in the steps array)
-  if (currentStepIndex < 0) {
-    // Fallback to a default step for unknown status
-    currentStepIndex = steps.length; // The new "Unknown" step will be at the end
-  }
+	// Handle invalid status (i.e., not found in the steps array)
+	if (currentStepIndex < 0) {
+		// Fallback to a default step for unknown status
+		currentStepIndex = 2; // The new "Unknown" step will be at the end
+	}
 
-  // State to trigger the animation on status change
-  const [animatedStep, setAnimatedStep] = useState(0); // Initially set to 0 for animation
-  const [currentWidth, setCurrentWidth] = useState(0); // Track the width of the progress bar
+	// State to trigger the animation on status change
+	const [animatedStep, setAnimatedStep] = useState(0); // Initially set to 0 for animation
+	const [currentWidth, setCurrentWidth] = useState(0); // Track the width of the progress bar
 
-  useEffect(() => {
-    // Trigger animation when status changes with a delay
-    const timer = setTimeout(() => {
-      setAnimatedStep(currentStepIndex); // Update the animated step index
-      setCurrentWidth((currentStepIndex / (steps.length)) * 100); // Set the target width
-    }, 500); // Delay of 500ms before starting the animation
+	useEffect(() => {
+		// Trigger animation when status changes with a delay
+		const timer = setTimeout(() => {
+		setAnimatedStep(currentStepIndex); // Update the animated step index
+		setCurrentWidth((currentStepIndex / (steps.length)) * 100); // Set the target width
+		}, 500); // Delay of 500ms before starting the animation
 
-    return () => clearTimeout(timer); // Cleanup timeout on unmount or change
-  }, [status, currentStepIndex]);
+		return () => clearTimeout(timer); // Cleanup timeout on unmount or change
+	}, [status, currentStepIndex]);
 
-  return (
-    <div className="rounded-lg mb-2 px-2 w-full">
-      <div className="w-full overflow-hidden">
-        <div className="flex items-center justify-between relative overflow-hidden">
-          {/* Progress Bar */}
-          <div className="absolute top-1/3 -translate-y-1/3 left-5 right-4 sm:left-4 sm:right-3 md:left-6 md:right-5 h-1 bg-gray-300 rounded-md">
-            <div
-              className="h-0.5 md:h-1 bg-red-400 transition-all duration-1000 ease-in-out"
-              style={{
-                width: `${currentWidth}%`, // Animate width from 0 to target value
-              }}
-            ></div>
-          </div>
-          {/* Steps */}
-          {steps.map((step, index) => (
-            <div key={index} className="flex flex-col items-center z-10 overflow-hidden">
-              <div
-                className={`flex flex-col items-center justify-center text-white font-bold shadow-md transition-all duration-500 ease-in-out text-sm sm:text-lg`}
-              >
-                <div
-                  className={`rounded-full flex items-center justify-center ${
-                    index <= animatedStep ? "bg-red-400" : "bg-gray-300"
-                  } w-10 h-10 transform transition-transform duration-500 ease-in-out`}
-                >
-                  {step.icon}
-                </div>
-                {!hiddenText && (
-                  <p
-                    className={`mt-2 text-xs font-semibold ${
-                      index <= animatedStep ? "text-gray-700" : "text-gray-400"
-                    }`}
-                  >
-                    {step.title}
-                  </p>
-                )}
-              </div>
-            </div>
-          ))}
-          {/* Fallback Unknown Status */}
-          {currentStepIndex === steps.length && (
-            <div className="flex flex-col items-center z-10 overflow-hidden">
-              <div
-                className={`flex flex-col items-center justify-center text-white font-bold shadow-md transition-all duration-500 ease-in-out text-sm sm:text-lg`}
-              >
-                <div
-                  className={`rounded-full flex items-center justify-center bg-yellow-500 w-10 h-10 transform transition-transform duration-500 ease-in-out`}
-                >
-                  ❓
-                </div>
-                {!hiddenText && (
-                  <p className="mt-2 text-xs font-semibold text-gray-700">Unknown Status</p>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="rounded-lg mb-2 px-2 w-full">
+			<div className="w-full overflow-hidden">
+				<div className="flex items-center justify-between relative overflow-hidden">
+					{/* Progress Bar */}
+					<div className="absolute top-1/3 -translate-y-1/3 left-5 right-4 sm:left-4 sm:right-3 md:left-6 md:right-5 h-1 bg-gray-300 rounded-md">
+						<div
+						className="h-0.5 md:h-1 bg-red-400 transition-all duration-1000 ease-in-out"
+						style={{
+							width: `${currentWidth}%`, // Animate width from 0 to target value
+						}}
+						></div>
+					</div>
+					{/* Steps */}
+					{steps.map((step, index) => (
+						<div key={index} className="flex flex-col items-center z-10 overflow-hidden">
+							<div
+								className={`flex flex-col items-center justify-center text-white font-bold shadow-md transition-all duration-500 ease-in-out text-sm sm:text-lg`}
+							>
+								<div
+									className={`rounded-full flex items-center justify-center ${
+										index <= animatedStep ? "bg-red-400" : "bg-gray-300"
+									} w-10 h-10 transform transition-transform duration-500 ease-in-out`}
+								>
+								{step.icon}
+								</div>
+								{!hiddenText && (
+									<p
+										className={`mt-2 text-xs font-semibold ${
+										index <= animatedStep ? "text-gray-700" : "text-gray-400"
+										}`}
+									>
+										{step.title}
+									</p>
+								)}
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DeliveryStatus;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -123,7 +123,7 @@ const Navbar = ({user}) => {
     }, [sessionData,sessionBagData,user,wishlist,bag]);
     return (
         <Fragment>
-            <div className="container font-kumbsan sticky top-0 2xl:w-[100%] xl:w-[100%] lg:w-[100%] mx-auto w-screen max-w-[100%] h-[80px] bg-neutral-100 contenthide z-40 ">
+            <div className="container font-kumbsan sticky top-0 2xl:w-[100%] xl:w-[100%] lg:w-[100%] mx-auto w-screen max-w-[100%] h-[80px] bg-neutral-100 contenthide z-40">
                 <div className='flex-row flex justify-between items-center w-screen h-full'>
                     <ul className=' h-full flex  font-semibold text-base md:text-[14px] text-[#282c3f] tracking-[.3px] uppercase'>
                         <Link className='w-max px-3 flex items-stretch hover:animate-vibrateScale' to="/">
